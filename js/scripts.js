f667eaed7ed11d3320b4149fd704dad37fd307e6
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-24 14:29:47 -0700

    Add input functionality on JS and form in HTML
    Co-authored-by: Drew Parker  <drewjparker91@gmail.com>
    Co-authored-by: Garrett Brown. <garrettpaulbrown@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 
-// Create a website where you can keep track of all the places you've been. Each destination should be an object with multiple properties, like location, landmarks, time of year, notes, etc. Display those properties when a user clicks on a place's name. Complete the business logic for your place object (including specs, which should go in your README). If you complete the business logic, you may work on adding a user interface. (See tonight's lessons for more.)
+// Each destination should be an object with multiple properties, like location, landmarks, time of year, notes, etc. 
+// Display those properties when a user clicks on a place's name. 
+// Complete the business logic for your place object (including specs, which should go in your README). 
+// If you complete the business logic, you may work on adding a user interface. (See tonight's lessons for more.)
 
 function PlacesWeveBeen() {
   this.destinations = [];
@@ -7,21 +10,43 @@ function PlacesWeveBeen() {
 }
 
 PlacesWeveBeen.prototype.addDestination  = function(destination) {
+  destination.id=this.assignId();
   this.destinations.push(destination)
 }
 
+PlacesWeveBeen.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
 
 
-function Destination(location, landmarks, timeOfYear, notes) {
+function Destination(location, landmarks, season, notes) {
   this.location = location;
   this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
+  this.season = season;
   this.notes = notes;
 }
 
 
+// let placesWeveBeen = new PlacesWeveBeen();
+// let destination = new Destination("Portland", "Moda Center", "Spring", "Cherry blossoms bloom");
+// let destination2 = new Destination("Seattle", "Gum Wall", "Winter", "sticky");
+// placesWeveBeen.addDestination(destination);
+// placesWeveBeen.addDestination(destination2);
+
+// User Interface Logic
+
 let placesWeveBeen = new PlacesWeveBeen();
-let destination = new Destination("Portland", "Moda Center", "Spring", "Cherry blossoms bloom");
-let destination2 = new Destination("Seattle", "Gum Wall", "Winter", "sticky");
-placesWeveBeen.addDestination(destination);
-placesWeveBeen.addDestination(destination2);
+
+$(document).ready(function() {
+  $("form#inputForm").submit(function(event) {
+    event.preventDefault();
+    const inputtedLocation = $("input#location").val();
+    const inputtedLandmarks = $("input#landmarks").val();
+    const inputtedSeason = $("input#season").val();
+    const inputtedNotes = $("input#notes").val();
+    let newDestination = new Destination(inputtedLocation, inputtedLandmarks, inputtedSeason, inputtedNotes);
+    placesWeveBeen.addDestination(newDestination);
+    console.log(placesWeveBeen.destinations);
+  });
+});
